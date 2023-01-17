de7587450c1dd0c08de29649b3566e5e35ecf84f
Renata Macedo <macedorenata.developer@gmail.com>
2023-01-16 23:03:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Habit from "./components/Habit";
+
+import "./styles/global.css";
+
+function App() {
+  return (
+    <>
+      <Habit completed={3} />
+      <Habit completed={20} />
+      <Habit completed={30} />
+    </>
+  );
+}
+
+export default App;
